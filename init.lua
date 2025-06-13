bc3bd6599ce9a7138c21d8568f58c08fa0be893f
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-12 22:47:38 -0500

    fix: automatic_enable for LSP client attachment

--- init.lua
+++ init.lua
@@ -684,17 +684,18 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
+        superhtml = {},
         --
 
         lua_ls = {
@@ -735,7 +736,7 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false, -- set to false because we are using mason-tool-installer
-        automatic_enable = false, -- no need to enable because we are using handlers
+        automatic_enable = true, -- no need to enable because we are using handlers
         handlers = {
           function(server_name)
             local server = servers[server_name] or {}
