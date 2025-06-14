cc14ef3e2cca4be9219c3c253f9b839590c6597e
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-13 21:09:46 -0500

    set up copilot chat; TODO: Keymaps for Copilot chat, add sources for blink autocomplete

--- init.lua
+++ init.lua
@@ -930,6 +930,20 @@ require('lazy').setup({
       require('copilot').setup {}
     end,
   },
+
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
