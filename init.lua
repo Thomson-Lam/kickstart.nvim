5981230729de40058bb880faa1ece281b93fa019
Thomson-Lam <thomson.lam327@gmail.com>
2025-06-16 21:11:00 -0500

    used nvim tree instead of neo tree

--- init.lua
+++ init.lua
@@ -145,6 +145,11 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+-- NOTE:: Nvim-tree config; disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.opt.termguicolors = true -- set 24 bit color; optional
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
@@ -504,18 +509,60 @@ require('lazy').setup({
   },
 
   -- minimal file tree for only when you need it
-  {
+  -- TODO: Check out mini.files later if you want to.
+
+  --NOTE: commented out neo-tree; using nvim-tree instead.
+
+  --[[ {
     'nvim-neo-tree/neo-tree.nvim',
-    --'nvim-neo-tree/neo-tree.nvim',
     branch = '3.0',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      --'nvim-tree/nvim-web-devicons',
+      --'nvim-tree/nvim-web-devicons', -- optional icons for file tree 
       'MunifTanjim/nui.nvim',
     },
     lazy = false, -- neo tree lazy loads self
-    ---[[ @module 'neo-tree' @type neotree.Config? --]]
+    ---[[ @module 'neo-tree' @type neotree.Config? 
     opts = {}, -- default for now
+  }, --]]
+
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      require('nvim-tree').setup {
+        disable_netrw = true,
+        hijack_netrw = true,
+        respect_buf_cwd = true,
+        sync_root_with_cwd = true,
+        view = {
+          relativenumber = true,
+          float = {
+            enable = true,
+            open_win_config = function()
+              local screen_w = vim.opt.columns:get()
+              local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+              local window_w = screen_w * 0.5
+              local window_h = screen_h * 0.8
+              local window_w_int = math.floor(window_w)
+              local window_h_int = math.floor(window_h)
+              local center_x = (screen_w - window_w) / 2
+              local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+              return {
+                border = 'rounded',
+                relative = 'editor',
+                row = center_y,
+                col = center_x,
+                width = window_w_int,
+                height = window_h_int,
+              }
+            end,
+          },
+          width = function()
+            return math.floor(vim.opt.columns:get() * 0.8)
+          end,
+        },
+      }
+    end,
   },
 
   -- oil.nvim for files
