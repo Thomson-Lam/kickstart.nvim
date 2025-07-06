86e49680e7f6ba4c6420e4ebb5507c4d73456821
Thomson-Lam <thomson.lam327@gmail.com>
2025-07-06 18:14:45 -0400

    added vim tmux navigation

--- init.lua
+++ init.lua
@@ -1054,6 +1054,24 @@ require('lazy').setup({
       signature = { enabled = true },
     },
   },
+  { -- Vim Tmux navigation
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<cr>' },
+    },
+  },
   --[[{
     'zbirenbaum/copilot.lua',
     -- optional = true, -- for manual activation for all AI features
